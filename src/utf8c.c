5edeac731734c09181edb77951e7e64654d2576a
Joakim Joensuu <joakim.joensuu@gmail.com>
2026-09-01 20:06:49 +0300

    Restore the RFC 3629 §3 octet sequence table comment.

--- src/utf8c.c
+++ src/utf8c.c
@@ -5,6 +5,21 @@
 #include <stdint.h>
 #include <string.h>
 
+/*
+ * RFC 3629 §3:
+ *
+ *     | Char. number range  |        UTF-8 octet sequence
+ *     |    (hexadecimal)    |              (binary)
+ *   --+---------------------+---------------------------------------------
+ *   1 | 0000 0000-0000 007F | 0xxxxxxx
+ *   2 | 0000 0080-0000 07FF | 110xxxxx 10xxxxxx
+ *   3 | 0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
+ *   4 | 0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+ *                                ^     +------------------------+
+ *                             initial       following octets
+ *                              octet
+ */
+
 enum utf8_initial_octet_leading_ones : uint8_t {
   utf8_1_initial_octet_leading_ones = 0,
   utf8_2_initial_octet_leading_ones = 2,
